a85053282aa566274bb52151baa1d03a968098aa
cnjboards <cnjboards@gmail.com>
2025-04-06 15:12:26 -0400

    Update lv_conf.h
    Increase memory to stop crashing

--- lib/mf_Lvgl/src/lv_conf.h
+++ lib/mf_Lvgl/src/lv_conf.h
@@ -50,7 +50,7 @@
 #if LV_MEM_CUSTOM == 0
     /*Size of the memory available for `lv_mem_alloc()` in bytes (>= 2kB)*/
     // reduce memory footprint - DAM
-    #define LV_MEM_SIZE (24U * 1024U)          /*[bytes]*/
+    #define LV_MEM_SIZE (48U * 1024U)          /*[bytes]*/
 
     /*Set an address for the memory pool instead of allocating it as a normal array. Can be in external SRAM too.*/
     #define LV_MEM_ADR 0     /*0: unused*/
